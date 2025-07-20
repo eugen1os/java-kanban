51d49e2e4b2487e1a7fe3d49f928962649a769df
eugen1os <eugen.tartasyuk@yandex.ru>
2025-07-20 22:53:34 +0300

    Полная замена на Task-manager.

---
+++ src/Epic.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Epic extends Task {
+    private List<Integer> subtaskIds = new ArrayList<>();
+
+    public Epic(int id, String name, String description) {
+        super(id, name, description, Status.NEW);
+    }
+
+    public List<Integer> getSubtaskIds() { return subtaskIds; }
+    public void addSubtaskId(int id) { subtaskIds.add(id); }
+    public void removeSubtaskId(int id) { subtaskIds.remove((Integer) id); }
+
+    @Override
+    public String toString() {
+        return "Epic{id=" + id + ", name='" + name + "', status=" + status + ", subtasks=" + subtaskIds + "}";
+    }
+}
---
+++ src/Subtask.java
@@ -0,0 +1,15 @@
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(int id, String name, String description, Status status, int epicId) {
+        super(id, name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() { return epicId; }
+
+    @Override
+    public String toString() {
+        return "Subtask{id=" + id + ", name='" + name + "', status=" + status + ", epicId=" + epicId + "}";
+    }
+}
